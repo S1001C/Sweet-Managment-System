cc7695d40295cc6a9bebb8437808e6f47f4fe070
S1001C <ayachanachoudhary@gmail.com>
2025-12-14 18:06:20 +0530

    changes to the file name

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const User = require("../models/User");
 
 const generateToken = (user) => {
   return jwt.sign(
