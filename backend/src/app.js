8a31b03d8fd1bc80e47742c69c658035327071c8
S1001C <ayachanachoudhary@gmail.com>
2025-12-14 20:00:49 +0530

    changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,10 +4,8 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
+  origin: "*",
 }));
-
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth.routes"));
